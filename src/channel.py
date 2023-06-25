ab6eddf275e7337eb05bc76af0412830354e417f
Евгений Евстратьев <levipupsik321@gmail.com>
2023-06-25 14:39:30 +0300

    json создается, channel_id поменять нельзя, падает на ошибке атрибуты все добавил вроде всё работает)

--- src/channel.py
+++ src/channel.py
@@ -8,11 +8,33 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
+        self.__channel_id = channel_id
         api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.id = channel['items'][0]['id']
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.id
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+
+        return youtube
+
+    def to_json(self, file_name):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        json_info = json.dumps(channel)
+        with open(file=file_name, mode='w') as file_name:
+            file_name.write(json_info)
