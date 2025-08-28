418de3721fb960c5b3b2a869d3c6be2d93a3e49e
Eugene Evstratev <levipupsik321@gmail.com>
2025-08-28 17:57:27 +0300

    Импортировал нужные библиотеки. Инициализатор класса + Метод print_info() работает корректно.

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+import os
+import json
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +8,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.build_object = build(serviceName='youtube', version='v3', developerKey=os.getenv('API_KEY'))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = json.dumps(self.build_object.channels().list(id=self.channel_id, part='snippet,statistics').execute(),
+                             indent=2, ensure_ascii=False)
+        print(channel)
